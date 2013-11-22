40362c1465f6966028ad417e04b2d344d5d9dedc
David Deharbe <deharbe@gmail.com>
2013-11-22 13:36:09 -0300

    Added docstrings for modules b2llvm and b2llvm.bproject. Prettyfied code in b2llvm.project, according to pylint.

--- PROTO/b2llvm/bproject.py
+++ PROTO/b2llvm/bproject.py
@@ -1,7 +1,18 @@
+"""Module responsible for representing B project settings.
+
+B project settings inform what are the developed and base machines. For the
+developed machine, the settings additionally record what is the corresponding
+implementation.
+
+"""
 from b2llvm.strutils import commas
 import xml.etree.ElementTree as ET
 
-class BProject:
+def text(elem, child):
+    """Returns text of XML element child."""
+    return elem.find(child).text
+
+class BProject(object):
     '''
     Class representing B projects settings.
     '''
@@ -11,23 +22,23 @@ class BProject:
         '''
         root = ET.parse(name).getroot()
         implements = root.findall("./developed")
-        self.implement = { e.find("./machine").text:e.find("./implementation").text
+        self.implement = { text(e, "./machine"):text(e, "./implementation")
                            for e in implements }
         foreign = root.findall("./base")
         self.developed = self.implement.keys()
         self.base = { e.find("./machine").text for e in foreign }
 
-    def is_developed(self, m):
+    def is_developed(self, mach):
         ''' Tests if a machine is a developed machine.
 
         Inputs:
-        - m: a machine name
+        - mach: a machine name
         Returns:
         True iff m is declared as developed.
         '''
-        return m in self.developed
+        return mach in self.developed
 
-    def is_base(self, m):
+    def is_base(self, mach):
         ''' Tests if a machine is a base machine.
 
         Inputs:
@@ -35,9 +46,9 @@ class BProject:
         Returns:
         True iff m is declared as base.
         '''
-        return m in self.base
+        return mach in self.base
 
-    def has(self, m):
+    def has(self, mach):
         ''' Tests if a machine belongs to a B project
 
         Inputs:
@@ -46,13 +57,18 @@ class BProject:
         True if m is a developed or a base machine declared in project,
         False otherwise.
         '''
-        return self.is_developed(m) or self.is_base(m)
+        return self.is_developed(mach) or self.is_base(mach)
 
-    def implementation(self, m):
-        assert self.is_developed(m)
-        return self.implement[m]
+    def implementation(self, mach):
+        """Gets implementation of machine."""
+        assert self.is_developed(mach)
+        return self.implement[mach]
+
+    def implements_str(self):
+        "Return list of strings formed by each machine and implementation."
+        return [ m + ":" + i for m, i in self.implement.items()]
 
     def __str__(self):
         return ("developed = {" + commas(self.developed) + "}\n" +
-                "implement = {" + commas([ m + ":" + i for m,i in self.implement.items()]) + "}\n" +
+                "implement = {" + commas(self.implements_str()) + "}\n" +
                 "base = {" + commas(self.base) + "}\n")
