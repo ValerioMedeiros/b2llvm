a0fefb9d8f2d57ce662541c6840ce6c2b642340a
David Deharbe <deharbe@gmail.com>
2013-11-22 12:44:28 -0300

    Prettyfied docstrings in b2llvm.ast module.

--- PROTO/b2llvm/ast.py
+++ PROTO/b2llvm/ast.py
@@ -21,34 +21,24 @@ BOOL = "BOOL"
 ### TERMINALS ###
 
 def make_const(name, typedef, value):
-    '''
-    Creates AST node for a B constant.
-    '''
+    """Creates an AST node for a B constant."""
     return { "kind": "Cons", "id": name, "type": typedef, "value" : value }
 
 
 def make_var(name, typedef, scope):
-    '''
-    Creates an AST node for a B variable.
-    '''
+    """Creates an AST node for a B variable."""
     return { "kind": "Vari", "id": name, "type": typedef, "scope": scope }
 
 def make_imp_var(name, typedef):
-    '''
-    Creates an AST node for a B concrete variable.
-    '''
+    """Creates an AST node for a B concrete variable."""
     return make_var(name, typedef, "Impl")
 
 def make_loc_var(name, typedef):
-    '''
-    Creates an AST node for a B local variable.
-    '''
+    """Creates an AST node for a B local variable."""
     return make_var(name, typedef, "Local")
 
 def make_arg_var(name, typedef):
-    '''
-    Creates an AST node for a B operation argument.
-    '''
+    """Creates an AST node for a B operation argument."""
     return make_var(name, typedef, "Oper")
 
 def make_intlit(value):
@@ -56,14 +46,17 @@ def make_intlit(value):
     Creates an AST node for a B integer literal.
 
     - Input: an integer value
+
     '''
     return { "kind": "IntegerLit",
              "value": str(value)}
 
 def make_boollit(value):
     '''
+    Creates an AST node for a Boolean literal.
+
     - Input: "FALSE" or "TRUE"
-    Creates a boolean literal.
+
     '''
     return { "kind": "BooleanLit",
              "value": value}
@@ -78,175 +71,121 @@ MAXINT = make_intlit(2147483647)
 ### COMPOSED EXPRESSIONS ###
 
 def make_term(operator, args):
-    '''
-    Creates an AST node for a B term.
-    '''
+    """Creates an AST node for a B term."""
     return { "kind": "Term", "op": operator, "args" : args }
 
 def make_succ(term):
-    '''
-    Creates an AST node for a B succ (successor) expression.
-    '''
+    """Creates an AST node for a B succ (successor) expression."""
     return make_term("succ", [term])
 
 def make_pred(term):
-    '''
-    Creates an AST node for a B pred (predecessor) expression.
-    '''
+    """Creates an AST node for a B pred (predecessor) expression."""
     return make_term("pred", [term])
 
 def make_sum(term1, term2):
-    '''
-    Creates an AST node for a B sum expression.
-    '''
+    """Creates an AST node for a B sum expression."""
     return make_term("+", [term1, term2])
 
 def make_diff(term1, term2):
-    '''
-    Creates an AST node for a B subtraction expression.
-    '''
+    """Creates an AST node for a B subtraction expression."""
     return make_term("-", [term1, term2])
 
 def make_prod(term1, term2):
-    '''
-    Creates an AST node for a B product expression.
-    '''
+    """Creates an AST node for a B product expression."""
     return make_term("*", [term1, term2])
 
 def make_comp(operator, arg1, arg2):
-    '''
-    Creates an AST node for a B comparison.
-    '''
+    """Creates an AST node for a B comparison."""
     return { "kind": "Comp", "op": operator, "arg1": arg1, "arg2": arg2 }
 
 def make_le(term1, term2):
-    '''
-    Creates an AST node for a B "lower or equal" expression.
-    '''
+    """Creates an AST node for a B "lower or equal" expression."""
     return make_comp("<=", term1, term2)
 
 def make_lt(term1, term2):
-    '''
-    Creates an AST node for a B "lower than" expression.
-    '''
+    """Creates an AST node for a B "lower than" expression."""
     return make_comp("<", term1, term2)
 
 def make_ge(term1, term2):
-    '''
-    Creates an AST node for a B "greater or equal" expression.
-    '''
+    """Creates an AST node for a B "greater or equal" expression."""
     return make_comp("<=", term1, term2)
 
 def make_gt(term1, term2):
-    '''
-    Creates an AST node for a B "greater than" expression.
-    '''
+    """Creates an AST node for a B "greater than" expression."""
     return make_comp(">", term1, term2)
 
 def make_eq(term1, term2):
-    '''
-    Creates an AST node for a B equality.
-    '''
+    """Creates an AST node for a B equality."""
     return make_comp("=", term1, term2)
 
 def make_neq(term1, term2):
-    '''
-    Creates an AST node for a B inequality.
-    '''
+    """Creates an AST node for a B inequality."""
     return make_comp("!=", term1, term2)
 
 def make_form(operator, args):
-    '''
-    Creates an AST node for a B formula.
-    '''
+    """Creates an AST node for a B formula."""
     return { "kind": "Form", "op": operator, "args" : args }
 
 def make_and(arg1, arg2):
-    '''
-    Creates an AST node for a B conjunction.
-    '''
+    """Creates an AST node for a B conjunction."""
     return make_form("and", [arg1, arg2])
 
 def make_or(arg1, arg2):
-    '''
-    Creates an AST node for a B disjunction.
-    '''
+    """Creates an AST node for a B disjunction."""
     return make_form("or", [arg1, arg2])
 
 def make_not(arg):
-    '''
-    Creates an AST node for a B negation.
-    '''
+    """Creates an AST node for a B negation."""
     return make_form("not", [arg])
 
 ### INSTRUCTIONS ###
 
 def make_skip():
-    '''
-    Creates an AST node for a B skip instruction.
-    '''
+    """Creates an AST node for a B skip instruction."""
     return { "kind": "Skip" }
 
 def make_beq(lhs, rhs):
-    '''
-    Creates an AST node for a B becomes equal instruction.
-    '''
+    """Creates an AST node for a B becomes equal instruction."""
     return { "kind": "Beq", "lhs": lhs, "rhs": rhs}
 
 def make_bin(lhs):
-    '''
-    Represents a becomes in susbtitution (but only the left hand side).
-    '''
+    """Represents a becomes in susbtitution (but only the left hand side)."""
     return { "kind": "Bin", "lhs": lhs}
 
 def make_blk(body):
-    '''
-    Creates an AST node for a B block instruction.
-    '''
+    """Creates an AST node for a B block instruction."""
     assert type(body) is list
     return { "kind": "Blk", "body": body }
 
 def make_var_decl(variables, body):
-    '''
-    Creates an AST node for a B variable declaration instruction.
-    '''
+    """Creates an AST node for a B variable declaration instruction."""
     assert type(variables) is list
     assert type(body) is list
     return { "kind": "VarD", "vars": variables, "body": body }
 
 def make_if_br(cond, body):
-    '''
-    Creates an AST node for a B if branch.
-    '''
+    """Creates an AST node for a B if branch."""
     assert type(body) is not list
     return { "kind": "IfBr", "cond": cond, "body": body}
 
 def make_if(branches):
-    '''
-    Creates an AST node for a B if instruction.
-    '''
+    """Creates an AST node for a B if instruction."""
     assert type(branches) is list
     return { "kind": "If", "branches": branches }
 
 def make_case_br(values, body):
-    '''
-    Creates an AST node for a B case branch.
-    '''
+    """Creates an AST node for a B case branch."""
     assert type(values) is list
     assert type(body) is not list
     return { "kind": "CaseBr", "val": values, "body": body }
 
 def make_case(expression, branches):
-    '''
-    Creates an AST node for a B case instruction.
-    '''
+    """Creates an AST node for a B case instruction."""
     assert type(branches) is list
     return { "kind": "Case", "expr": expression, "branches": branches }
 
 def make_while(cond, body):
-    '''
-    Creates an AST node for a B while instruction.
-    '''
+    """Creates an AST node for a B while instruction."""
     assert type(body) is list
     return { "kind": "While", "cond": cond, "body": body }
 
@@ -259,8 +198,7 @@ def make_call(operator, inp, out, inst=None):
     inp: a sequence of inputs
     out: a sequence of outputs
     inst: an element of an imported clause (optional)
-    - Output:
-    an operation call
+
     '''
     return { "kind": "Call", "op": operator, "inp": inp, "out": out,
              "inst": inst }
@@ -268,9 +206,7 @@ def make_call(operator, inp, out, inst=None):
 ### COMPONENT ###
 
 def make_oper(name, inp, out, body):
-    '''
-    Creates an AST node to represent a B operation.
-    '''
+    """Creates an AST node to represent a B operation."""
     assert type(inp) is list
     assert type(out) is list
     assert type(body) is not list
@@ -290,9 +226,7 @@ def make_import(mach, prefix = None):
     return { "kind": "Module", "mach": mach, "pre": prefix }
 
 def make_implementation(name, imports, consts, variables, init, ops):
-    '''
-    Creates an AST node for a B implementation module.
-    '''
+    """Creates an AST node for a B implementation module."""
     assert type(imports) is list
     assert type(consts) is list
     assert type(vars) is list
